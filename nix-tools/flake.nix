696caabec1ceae4c341f3f465e3509cef433ab8b
Andrea Bedini <andrea.bedini@tweag.io>
2023-02-09 13:41:08 +0800

    Build nix-tools statically

--- nix-tools/flake.nix
+++ nix-tools/flake.nix
@@ -1,43 +1,74 @@
 {
-  # This is a template created by `hix init`
-  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
-  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils, haskellNix }:
-    let
-      ci = (builtins.fromTOML (__readFile ./ci.toml)).dimensions;
-    in
-      flake-utils.lib.eachSystem ci.os (system:
-      let
-        compilers = builtins.filter
-          (x: !__elem "${system}.${x}" ci.disable)
-          (ci.compiler);
-        overlays = [ haskellNix.overlay
-          (final: prev: {
-            hixProject =
-              final.haskell-nix.hix.project {
-                src = ./.;
-                evalSystem = "x86_64-darwin";
-                compiler-nix-name = __head compilers;
-              };
-          })
-        ];
-        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
-        flake = pkgs.hixProject.flake {
-          variants = pkgs.lib.genAttrs (__tail compilers)
-            (x: { compiler-nix-name = pkgs.lib.mkForce x; });
-        };
-      in flake // {
-        legacyPackages = pkgs;
-      });
+  inputs = {
+    haskell-nix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskell-nix/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
+  outputs = { self, nixpkgs, flake-utils, haskell-nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # pkgs = let
+        pkgs = import nixpkgs {
+          inherit system;
+          inherit (haskell-nix) config;
+          overlays = [ haskell-nix.overlay ];
+        };
+
+        # don't use cabalProject here, it's too strict and accidentally evaluates IDF for other architectures 
+        project = pkgs: pkgs.haskell-nix.cabalProject' rec {
+          src = ./.;
+          compiler-nix-name = "ghc925";
+        };
+
+        flake = pkgs: (project pkgs).flake { };
+
+        mkBinaryTarball = drv:
+          pkgs.runCommand drv.name { nativeBuildInputs = [ pkgs.gnutar ]; } ''
+            mkdir -p $out
+            tar czv -C ${drv} --dereference . > $out/${drv.name}.tgz
+            mkdir -p $out/nix-support
+            echo file binary-dist $out/${drv.name}.tgz > $out/nix-support/hydra-build-products
+          '';
+
+        projectComponents = prj: comp-type:
+          pkgs.lib.concatMap
+            (package:
+              if package != null then
+                pkgs.lib.attrValues (package.components.${comp-type} or { })
+              else
+                [ ])
+            (pkgs.lib.attrValues prj.hsPkgs);
+
+      in
+      pkgs.haskell-nix.haskellLib.combineFlakes ""
+        (
+          { "native" = flake pkgs; }
+          // pkgs.lib.optionalAttrs (pkgs.stdenv.hostPlatform.isLinux && pkgs.stdenv.hostPlatform.isx86_64)
+            (pkgs.lib.listToAttrs
+              (map (pkgs': pkgs.lib.nameValuePair "${pkgs'.hostPlatform.config}" (flake pkgs'))
+                [ pkgs.pkgsCross.musl64 pkgs.pkgsCross.aarch64-multiplatform-musl ]))
+        )
+      // {
+        # inherit project;
+        # constituents = projectComponents project "exes";
+        # hydraJobs = flake.hydraJobs // {
+        #   binary = mkBinaryTarball (pkgs.symlinkJoin {
+        #     name = "nix-tools";
+        #     paths = projectComponents project "exes";
+        #   });
+        # };
+      });
   # --- Flake Local Nix Configuration ----------------------------
   nixConfig = {
-    # This sets the flake to use the IOG nix cache.
-    # Nix should ask for permission before using it,
-    # but remove it here if you do not want it to.
-    extra-substituters = ["https://cache.iog.io"];
-    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+    extra-substituters = [
+      "https://cache.iog.io"
+      "https://cache.zw3rk.com"
+    ];
+    extra-trusted-public-keys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
+    ];
     allow-import-from-derivation = "true";
   };
 }
